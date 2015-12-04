d1c136a9105bb853d693a58ee50e3b35879ea8fc
fdsnsf <sun_fan0825@sina.com>
2015-12-05 00:19:26 +0800

    3 sum

--- algorithms/python/3Sum.py
+++ algorithms/python/3Sum.py
@@ -19,11 +19,24 @@ def threeSum(nums):
 
 	length = len(nums)
 	for i in range(length):
+		for j in range (i+1, len(nums)):
+			r = 0 - nums[i] - nums[j]
+			if (nMap[nums[j]]>1 and r == nums[j]) or (r in nMap and r > nums[j]):
+				if nums[i] == nums[j] and r == 0 and nMap[0]<3:
+					continue
+				l = [nums[i], nums[j], r]
+				if l not in result:
+					result.append(l)
+	return result
 
-		for j in range (i, len(nums)):
-			r = 0 - nums[i] + nums[j]
-			if (nMap[nums[j]]>1 and r == nums[j]):
-				result.append([nums[i], nums[j], r])
+print threeSum([-1, 0, 1, 2, -1, -4])
+print threeSum([1,2,-2,-1])
+print threeSum([1,1,-2])
+print threeSum([0,0,0])
+print threeSum([-1,0,1,0])
+
+
+
 
 
 		
